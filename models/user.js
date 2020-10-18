f7be9a99aca5edef487f172f8c0c45b446ba995d
Diego Castillo <61103323+LearnLab@users.noreply.github.com>
2020-10-18 12:36:18 -0500

    Create user schema with password creation and verification methods

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 // User model
 const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
 	name: {
@@ -15,8 +17,36 @@ const userSchema = mongoose.Schema({
 		required: true,
 		type: String, 
 		unique: true
-	}
+	},
+	hash: String,
+	salt: String
 });
+
+userSchema.methods.setPassword = (password) => {
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.hash = crypto
+		.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+		.toString('hex');
+};
+
+userSchema.methods.validPassword = (password) => {
+	const hash = crypto
+		.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+		.toString('hex');
+
+	return this.hash === hash;
+};
+
+userSchema.methods.generateJWT = () => {
+	const expiry = new Date();
+	expiry.setDate(expiry.getDate() + 7);
+	return jwt.sign({
+		_id: this._id,
+		email: this.email,
+		name: this.name,
+		exp: parseInt(expiry.getTime() / 1000)
+	}, process.env.JWT_SECRET);
+};
 
 // This won't be imported but fetched from mongoose
 mongoose.model('User', userSchema);

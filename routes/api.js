fe57877b472af5bdd5598e1fcefb576ec2462b1d
Diego Castillo <61103323+LearnLab@users.noreply.github.com>
2020-11-15 16:21:11 -0500

    Add authentication to put and delete methods

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('express-jwt');
+const auth = jwt({
+	secret: process.env.JWT_SECRET,
+	requestProperty: 'payload', 
+	algorithms: ['HS256']
+});
 const usersController = require('../controllers/users');
 const authController = require('../controllers/authentication');
 
@@ -12,9 +18,11 @@ router
 
 router
 	.route('/users/:user')
+	// Authentication optional
 	.get(usersController.getUser)
-	.put(usersController.updateUser)
-	.delete(usersController.deleteUser);
+	// These require authentication
+	.put(auth, usersController.updateUser)
+	.delete(auth, usersController.deleteUser);
 
 // Export routes
 module.exports = router;
